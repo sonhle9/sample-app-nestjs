7b2f87e5fe49f93fa59ecb488999c2f7774aac51
sonhle9 <nguyenducmanh13021995@gmail.com>
2021-12-28 08:44:37 +0700

    set cors for https://github.com/sonhle9/sample_app_reactjs, port 3002

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins = [
+    'http://localhost:3000', 
+    'http://localhost:3001',
+    'http://localhost:3002',
+    'http://localhost:4200',
+    'http://localhost:8080',
+    'https://railstutorialreact.herokuapp.com',
+    'https://boiling-depths-89533.herokuapp.com',
+    'https://sample-app-nextjs.vercel.app',
+  ];
+  app.enableCors({credentials: true, origin: allowedOrigins});
   await app.listen(3001);
 }
 bootstrap();
